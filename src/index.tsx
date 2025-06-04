836530cadd1539efd919b1d98b4def4ec5d44539
ketace06 <ismail.omaramin06@gmail.com>
2025-06-04 07:54:57 +0200

    fix: deploy homepage first instead of todoapp

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TodoApp from "./TodoApp";
-import "./components/assets/App.css";
-
-const rootEl = document.getElementById("root");
-if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(
-    <React.StrictMode>
-      <TodoApp />
-    </React.StrictMode>,
+import { BrowserRouter } from "react-router-dom";
+import HomePage from "./components/HomePage";
+function TodoApp() {
+  return (
+    <BrowserRouter basename="/Todo-list-React">
+      <HomePage />
+    </BrowserRouter>
   );
 }
+
+export default TodoApp;
